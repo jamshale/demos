62a8f49ee405f9ea98c04e711df029b9d2d59401
jamshale <jamiehalebc@gmail.com>
2019-09-10 10:40:51 -0700

    changed one to pong

--- src/main/resources/static/js/one.js
+++ src/main/resources/static/js/one.js
@@ -1,57 +1,26 @@
-var X, Y, ballX, ballY;
-var H = 100;
-var W = 100;
-$( document ).on( "mousemove", function( event ) {
-    X = event.pageX;
-    Y = event.pageY;
-    ballX = $('#ball').width()/2
-    ballY = $('#ball').height()/2
-    xyMod = (X+Y) % 200;
-    xMod = X % 200;
-    yMod = Y % 200;
-    $('#ball').css({'position':'absolute',
-                    'left': X-ballX,
-                    'top':Y-ballY,
-                    'background-color': `rgb(${xyMod}, ${xMod} , ${yMod} )`});
-    // console.log("pageX: " + event.pageX + ", pageY: " + event.pageY);
-    // $( "#log" ).text( "pageX: " + event.pageX + ", pageY: " + event.pageY );
-});
+var H = $(document).height()
+var W = $(document).width()
+var steps = 100;
 
-$( document ).on( "mousedown", function( event ) {
+function start(){
+    $('.btn-success').remove();
+    var hRand = Math.random(0,1);
+    var target = H*hRand;
+    var leftOrRight = Math.random(0,1);
+    var wDist = W/2;
+    var hDist = target/2;
+    var wInc = wDist/steps;
+    var hInc = hDist/steps;
 
-
-    var start =  $.now()
-    var now;
-    timeoutDown = setInterval(function(){
-        now = $.now();
-        H += (now-start)/1000
-        W += (now-start)/1000
-        $('#ball').css({'height':H, 'width':W})
-    })
-   
-});
-
-$( document ).on( "mouseup", function( event ) {
-    clearInterval(timeoutDown);
-
-    // var start =  $.now()
-    // var now;
-    // timeoutDown = setInterval(function(){
-    //     now = $.now();
-    //     H -= (now-start)/10000
-    //     W -= (now-start)/10000
-    //     $('#ball').css({'height':H, 'width':W})
-    // })
-})
-
-// $( document ).on( "mouseout", function( event ) {
-//     clearInterval(timeoutDown);
-// })
-
-$( document ).on( "click", function() {
-    // console.log("xMod = " + xMod + " yMod = " + yMod );
-    // console.log("ballX = " + ballX + " ballY = " + ballY );
-    // console.log("X = " + X + " Y = " + Y );
-
-});
-
+    for(var i = 0; i < steps; i++){
+        
+            var position = $('#ball').position();
+            console.log(position.top)
+            console.log(position.left)
+            $("#ball").parent().css({position: 'relative'});
+            $("#ball").css(`{top: ${position.top + hInc}, left: ${position.left + wInc}, position:'absolute'}`);
+            // $('#ball').css('left', `${position.left + wInc}`)
+            // $('#ball').css('top', `${position.top + hInc}`)
+        
+    }
+}
